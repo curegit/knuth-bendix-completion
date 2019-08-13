32303157d3f9f18b69c6d42b6507de41ae8fcf80
Cure Git <37978051+curegit@users.noreply.github.com>
2019-08-13 11:31:16 +0900

    Ignore Dune if not available

--- clean.sh
+++ clean.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-dune clean
 rm -r -f _byte
 rm -r -f _native
 rm -f ./*.cmi
@@ -9,3 +8,4 @@ rm -f ./*.cma
 rm -f ./*.o
 rm -f ./*.cmx
 rm -f ./*.cmxa
+which dune > /dev/null && dune clean
